8c0959d344fe4c48b220de7a4f69d0f7571bee2c
samuelsvenssonNTI2C <150041300+samuelsvenssonNTI2C@users.noreply.github.com>
2025-01-21 16:46:30 +0100

    routes!

--- app.rb
+++ app.rb
@@ -1,5 +1,51 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sessions'
 require 'slim'
 require 'sqlite3'
-require 'sinatra/reloader'
-require 'becrypt'
+require 'bcrypt'
+
+get('/') do
+	redirect('/home')
+end
+
+get('/home') do
+	slim(:home)
+end
+
+post('/user/login') do
+
+end
+
+get('/user/login') do
+	slim(:'user/index')
+end
+
+post('/user/create') do
+	
+end
+
+get('user/new') do
+	slim(:'user/new')
+end
+
+get('user/show/:id') do
+	userId = params[:id]
+	slim(:'user/show', locals:{id:userId})
+end
+
+get('/images') do
+
+end
+
+post('/images/create') do
+
+end
+
+get('/images/new') do
+
+end
+
+get('/images/show/:id') do
+
+end
